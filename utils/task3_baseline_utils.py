4721ce67684743536b502bbb5e542cb767fbea87
Wang Ke (kw24z021) <kw24z021@gnode34.ubelix.unibe.ch>
2026-01-03 15:46:49 +0100

    Cleanup Task1, finished Task 3-subtask2

--- utils/task3_baseline_utils.py
+++ utils/task3_baseline_utils.py
@@ -3,13 +3,41 @@ import pandas as pd
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 import numpy as np
 
-def store_model_metrics_manual(y_true, y_pred, unique_labels, results_path):
+def store_model_metrics_manual(y_true, y_pred, results_path):
     metrics = {}
-    metrics["f1_weighted"] = f1_score(y_true, y_pred, labels=list(sorted(unique_labels)), average='weighted', zero_division=0)
+
+    metrics["f1_weighted"] = f1_score(
+        y_true,
+        y_pred,
+        average='weighted',
+        zero_division=0
+    )
+
     metrics["accuracy"] = accuracy_score(y_true, y_pred)
-    metrics["precision"] = precision_score(y_true, y_pred, average='weighted', zero_division=0)
-    metrics["recall"] = recall_score(y_true, y_pred,average='weighted', zero_division=0)
+
+    metrics["precision"] = precision_score(
+        y_true,
+        y_pred,
+        average='weighted',
+        zero_division=0
+    )
+
+    metrics["recall"] = recall_score(
+        y_true,
+        y_pred,
+        average='weighted',
+        zero_division=0
+    )
+    
+    metrics["f1_macro"] = f1_score(
+    y_true,
+    y_pred,
+    average='macro',
+    zero_division=0
+)
+
     pd.DataFrame([metrics]).to_csv(results_path, index=False)
+
     
 
 # TODO the following is not calculating values properly - all zeros
